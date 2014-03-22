610e63f62717fd4c692dd93208e3f12e9a416eb9
Eirik Lied <eiriklied@gmail.com>
2014-03-22 18:22:56 +0100

    Create new board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,16 @@
 class BoardsController < ApplicationController
-  before_filter :load_board
-  
+  before_filter :load_board, except: :create
+
   def show
+  end
+
+  def create
+    @board = Board.new(params.require(:board).permit(:name))
+    if @board.save
+      redirect_to board_url(@board)
+    else
+      redirect_to root_url
+    end
   end
 
   def install
--- spec/features/create_board_spec.rb
+++ spec/features/create_board_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+describe 'create a board' do
+
+  it 'should let users create boards from the front page' do
+    visit root_path
+    fill_in 'board_name', with: 'Team 1 board'
+    click_on 'create_board'
+
+    expect(Board.count).to eq(1)
+    @board = Board.last
+
+    expect(current_path).to eq(board_path(@board))
+  end
+end
