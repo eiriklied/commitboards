1db6295ccbfaf926b57b8ca4db10113190c042f5
Eirik Lied <eiriklied@gmail.com>
2014-05-02 13:13:10 +0200

    small performance optimization: O(n)+1

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,7 +2,7 @@ class Commit < ActiveRecord::Base
   belongs_to :board
   belongs_to :user
 
-  has_many :comments, -> { order('created_at asc') }
+  has_many :comments, -> { order('created_at asc').includes(:user) }
 
   validates :project, :sha, :committed_at,
              presence: true
