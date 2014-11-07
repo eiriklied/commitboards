e5119ab2312974e364904a960e7e37431d7e232e
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-11-07 13:57:51 +0100

    test speedup

--- spec/controllers/board_controller_spec.rb
+++ spec/controllers/board_controller_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 describe BoardsController do
-    
-  before :each do
-    create_list :user, 4
+  let(:board) { Board.first }
+  
+  before :all do
+    create :user_with_board
+    create_list :user_with_commits, 4
   end
-
-  let(:board)       { create :board, owner: User.first }
   
   describe 'GET show' do
     
-    before :each do
-      4.times do |index| 
-        create_list :commit, (index+1)*2, board: board, user: User.find(index+1)
-      end
-    end
-
     it 'responds with map of top three users with the most commits the last week' do
       get :show, id: board.key
       expect(assigns(:commits_pr_user).length).to eq(3)
@@ -23,13 +17,13 @@ describe BoardsController do
 
     it 'sets the top user correctly' do
       get :show, id: board.key
-      expect(assigns(:commits_pr_user).keys[0]).to eq(User.fourth)
+      expect(assigns(:commits_pr_user).keys[0]).to eq(User.last)
       expect(assigns(:commits_pr_user).values[0]).to eq(8) 
     end 
     
     it 'sets the third user correctly' do
       get :show, id: board.key
-      expect(assigns(:commits_pr_user).keys[2]).to eq(User.second)
+      expect(assigns(:commits_pr_user).keys[2]).to eq(User.third)
       expect(assigns(:commits_pr_user).values[2]).to eq(4) 
     end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,23 @@ FactoryGirl.define do
     email 'ola@nordmann.no'
     github_url 'test'
     avatar_url 'test'
-  end
 
+    factory :user_with_board do
+      after(:create) do |user|
+        create :board, owner: user
+      end
+    end
+
+    factory :user_with_commits do
+      
+      transient do
+        sequence(:count) { |n| n*2 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list :commit, evaluator.count, user: user, board: Board.first
+      end
+
+    end
+  end
 end
